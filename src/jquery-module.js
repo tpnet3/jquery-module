62cbf107e7018f3d06b0ab9c3a8bb93449b72ffb
tpnet3 <tpnet3@Primary-PC.ZIO_Router>
2016-10-22 12:37:07 +0900

    commmit

---
+++ src/jquery-module.js
@@ -0,0 +1,47 @@
+jQuery.module = {
+    _loaded: {},
+    _dir: ""
+};
+
+jQuery.fn.module = function(moduleUri, options, callback) {
+    if (moduleUri.slice(-3) == ".js") {
+        moduleUri = moduleUri.slice(0, moduleUri.length - 3);
+    }
+
+    var moduleName = moduleUri.split("/");
+    moduleName = moduleName[moduleName.length - 1];
+
+    return this.each(function() {
+        var _self = this;
+
+        var loadSuccess = function() {
+            if (jQuery.module[moduleName]) {
+                new jQuery.module[moduleName](_self, options);
+            }
+        }
+
+        if (jQuery.module._loaded[moduleName]) {
+            loadSuccess();
+        } else {
+            jQuery.module._loaded[moduleName] = true;
+            $(document).trigger("jqueryModuleLoadStart", [moduleName]);
+
+            $.getScript(jQuery.module._dir + moduleUri + ".js")
+                .done(function(script, textStatus) {
+                    loadSuccess();
+                    $(document).trigger("jqueryModuleLoadSuccess", [moduleName]);
+
+                    if (typeof callback == "function") {
+                        callback(true);
+                    }
+                })
+                .fail(function(jqxhr, settings, exception) {
+                    $(document).trigger("jqueryModuleLoadFail", [moduleName]);
+
+                    if (typeof callback == "function") {
+                        callback(false);
+                    }
+                });
+        }
+    });
+}
---
+++ tests/helloworld/helloworld.js
@@ -0,0 +1,3 @@
+jQuery.module.helloworld = function(_self, options) {
+    $(_self).html(options.text);
+}
